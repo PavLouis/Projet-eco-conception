51f4a4c7d7c94ee5ed6ac602d3ceec8694be4772
jude marzat <jude.marzat@epitech.eu>
2025-02-05 19:01:16 +0100

    On parle de nous

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './NavBar/NavBar'
 import Lechef from './Lechef/Lechef'
 import NosPatisseries from './NosPatisseries/NosPatisseries'
+import Onparledenous from './Onparledenous/Onparledenous'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <Lechef />
       <NosPatisseries />
+      <Onparledenous />
       <p>Hello!</p>
     </div>
   )
