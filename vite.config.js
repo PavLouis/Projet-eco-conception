074ea361d122e4254f540d9fa53cabc8a5ec8df3
pavlo <louis.pavlovsky@epitech.eu>
2025-02-19 13:43:29 +0100

    [ADD]: vite Image optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    ViteImageOptimizer({
+      exclude: ['**/*.svg'],
+      test: /\.(jpe?g|png|webp)$/i,
+      jpg: {
+        quality: 80,
+        mozjpeg: true,
+      },
+      png: {
+        quality: 80,
+      },
+      webp: {
+        lossless: false,
+        quality: 80,
+      }
+
+    }),
     VitePWA({ 
       registerType: 'autoUpdate',
       includeAssets: ['**/*.{svg,png,jpg,jpeg}'],
@@ -28,4 +45,12 @@ export default defineConfig({
     }),
 
   ],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true, // Supprimer console.log en production
+        drop_debugger: true
+      }
+    },
+  }
 })
